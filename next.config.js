e41e697266fffd1745a5f07c1b4d07f28764e5bf
Peter <phefland@gmail.com>
2019-11-07 09:09:39 -0300

    Building pages for login and signup forms. Modelling user, and signup user with JWT + cookies. Validate post content on server. Add login functionality. Create user cart on signup.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV:
       "mongodb+srv://pete:22512516@reactreserve-1jtue.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "ThisIsTopSecret",
     CLOUDINARY_URL:
       "https://api.cloudinary.com/v1_1/petereflandcloudinary/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
