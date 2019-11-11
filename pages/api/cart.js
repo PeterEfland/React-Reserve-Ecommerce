a3b0531a2a5ed4521ecbe2e604ed158440fbce84
Peter <phefland@gmail.com>
2019-11-10 22:59:10 -0300

    strugling with deploying to Zeit

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import Cart from "../../models/Cart";
 import connectDb from "../../utils/connectDb";
+import Product from "../../models/Product";
 
 connectDb();
 
@@ -35,7 +36,7 @@ async function handleGetRequest(req, res) {
     );
     const cart = await Cart.findOne({ user: userId }).populate({
       path: "products.product",
-      model: "Product"
+      model: Product
     });
     res.status(200).json(cart.products);
   } catch (error) {
@@ -97,7 +98,7 @@ async function handleDeleteRequest(req, res) {
       { new: true }
     ).populate({
       path: "products.product",
-      model: "Product"
+      model: Product
     });
     res.status(200).json(cart.products);
   } catch (error) {
